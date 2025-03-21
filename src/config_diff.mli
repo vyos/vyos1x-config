0ed398e75c0e4ceae8461f098a7ee2e9c7efeec2
John Estabrook <jestabro@vyos.io>
2025-03-21 15:54:06 -0500

    T7272: expose config_tree clone

--- src/config_diff.mli
+++ src/config_diff.mli
@@ -41,6 +41,7 @@ exception Incommensurable
 exception Empty_comparison
 exception Nonexistent_child
 
+val clone : ?recurse:bool -> ?set_values:string list option -> Config_tree.t -> Config_tree.t ->string list -> Config_tree.t
 val diff_tree : string list -> Config_tree.t -> Config_tree.t -> Config_tree.t
 val show_diff : ?cmds:bool -> string list -> Config_tree.t -> Config_tree.t -> string
 val tree_union : Config_tree.t -> Config_tree.t -> Config_tree.t
